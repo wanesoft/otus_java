cabbb3e8198759895ea1261d4413f62f3e739055
Ivan.Marochkin <Ivan.Marochkin@fleetcor.ru>
2022-05-21 22:30:02 +0300

    Impl

--- 01/src/main/java/ru/otus/spring/Main.java
+++ 01/src/main/java/ru/otus/spring/Main.java
@@ -1,20 +1,23 @@
 package ru.otus.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.spring.domain.Person;
-import ru.otus.spring.service.PersonService;
+import ru.otus.spring.domain.Question;
+import ru.otus.spring.service.QuestionsService;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        PersonService service = context.getBean(PersonService.class);
-        Person ivan = service.getByName("Ivan");
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+        QuestionsService service = context.getBean(QuestionsService.class);
+        while (true) {
+            Question cur = service.getNext();
+            if (cur == null) {
+                break;
+            }
+            System.out.println(cur);
+            System.out.println("---");
+        }
 
-        // Данная операция, в принципе не нужна.
-        // Мы не работаем пока что с БД, а Spring Boot сделает закрытие за нас
-        // Подробности - через пару занятий
         context.close();
     }
 }
