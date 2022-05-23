18b7af57d062ff0b00d489e94dc8cfd3d8d13c04
Ivan.Marochkin <Ivan.Marochkin@fleetcor.ru>
2022-05-23 19:59:20 +0300

    Preps for PR

--- 01/src/main/java/ru/otus/spring/dao/QuestionsDao.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.dao;
-
-import ru.otus.spring.domain.Question;
-
-public interface QuestionsDao {
-
-    Question getNext();
-}
--- 01/src/main/java/ru/otus/spring/service/QuestionsService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Question;
-
-public interface QuestionsService {
-
-    Question getNext();
-}
