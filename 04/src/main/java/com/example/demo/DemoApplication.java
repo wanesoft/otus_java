45c4647e58e2d89d8e377475a41c0264edb1fcc5
Ivan.Marochkin <Ivan.Marochkin@fleetcor.ru>
2022-06-19 15:31:28 +0300

    Fix

--- 04/src/main/java/com/example/demo/DemoApplication.java
+++ 04/src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.service.QuestionsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @SpringBootApplication
 public class DemoApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-//	}
-
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		QuestionsService service = context.getBean(QuestionsService.class);
-		service.run();
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- 04/src/test/java/com/example/demo/DemoApplicationTests.java
+++ 04/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,36 +1,34 @@
 package com.example.demo;
 
 import com.example.demo.config.ApplicationProperty;
-import com.example.demo.config.TestConfig;
-import com.example.demo.service.QuestionsService;
-import com.example.demo.service.UserImpl;
-import com.example.demo.service.UserTestImpl;
+import com.example.demo.dao.QuestionsDaoSimple;
+import com.example.demo.service.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Import(TestConfig.class)
 @SpringBootTest(properties = {
 		InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
-		ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
-})
+		ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false",
+		}, classes = {UserTestImpl.class,
+		QuestionsServiceImpl.class,
+		ApplicationProperty.class,
+		QuestionsDaoSimple.class})
 class DemoApplicationTests {
+
+	@Autowired
+	QuestionsServiceImpl service;
+
+	@Autowired
+	ApplicationProperty props;
 
 	@Test
 	void simpleTest() {
-		ApplicationContext context = SpringApplication.run(DemoApplication.class);
-		QuestionsService service = context.getBean(QuestionsService.class);
-		ApplicationProperty props = context.getBean(ApplicationProperty.class);
 		int countCorrectAnswers = service.run();
 		assertTrue(countCorrectAnswers >= props.getMinCorrectAnswers());
 	}
